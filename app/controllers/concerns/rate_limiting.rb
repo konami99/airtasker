c180596431340951544c7eaf1120786def62e10f
konami99 <richard@flatmates.com.au>
2017-06-15 23:13:43 +1000

    basic template for RateLimiting

--- app/controllers/concerns/rate_limiting.rb
+++ app/controllers/concerns/rate_limiting.rb
@@ -1,13 +1,19 @@
 module RateLimiting
   extend ActiveSupport::Concern
 
+  REQUEST_LIMIT = 100
+
   included do
-    before_action :check_request_ip
+    before_action :block_request, if: :too_many_requests?
 
     private
 
-    def check_request_ip
+    def too_many_requests?
+      true
+    end
 
+    def block_request
+      render status: 429, json: {}
     end
   end
 
