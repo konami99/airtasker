6b679a5bafda50fda42d6b3e05c08fd5b9bf644c
konami99 <richard@flatmates.com.au>
2017-06-15 22:39:58 +1000

    set default response format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_default_format
+
+  private
+
+  def set_default_format
+    request.format = :json if params[:format].nil?
+  end
 end
