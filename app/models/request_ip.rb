44aab07a5be9693bb4a75de879073303321d6a27
konami99 <richard@flatmates.com.au>
2017-06-16 11:01:01 +1000

    refactor RequestIp

--- app/models/request_ip.rb
+++ app/models/request_ip.rb
@@ -1,8 +1,18 @@
 class RequestIp < ApplicationRecord
   validates :ip_address, :count, :started_at, presence: true
 
-  def to_string
-    IPAddr.new(ip_address, Socket::AF_INET).to_s
+  class << self
+    def to_integer(string)
+      IPAddr.new(string).to_i
+    end
+
+    def to_string(integer)
+      IPAddr.new(integer, Socket::AF_INET).to_s
+    end
+  end
+
+  def ip_address
+    IPAddr.new(self[:ip_address], Socket::AF_INET).to_s
   end
 
   def ip_address=(string)
