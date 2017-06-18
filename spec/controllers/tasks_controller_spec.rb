7f6a3bfd436b66957fd0f5077d4fbc0ac6655c09
konami99 <richard@flatmates.com.au>
2017-06-18 22:50:35 +1000

    fix test

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,13 +2,19 @@ require "rails_helper"
 
 RSpec.describe TasksController do
   describe "GET #create" do
+    def source
+      "tasks#create"
+    end
+
     before :each do
       allow_any_instance_of(ActionDispatch::Request).to receive(:remote_ip).and_return("60.241.160.246")
+      allow(controller).to receive(:source).and_return("tasks#create")
     end
 
     it "returns 200, if there is no request count" do
       create \
         :request_ip,
+        source: source,
         ip_address: "1.1.1.1",
         count: 101
       post :create, params: { task: attributes_for(:task) }, format: :json
@@ -18,6 +24,7 @@ RSpec.describe TasksController do
     it "returns 200, if there is no request count for current hour" do
       create \
         :request_ip,
+        source: source,
         ip_address: "60.241.160.246",
         started_at: (Time.current - 1.hour).beginning_of_hour,
         count: 101
@@ -28,6 +35,7 @@ RSpec.describe TasksController do
     it "returns 200, if request count less than 100" do
       create \
         :request_ip,
+        source: source,
         ip_address: "60.241.160.246",
         count: 99
       post :create, params: { task: attributes_for(:task) }, format: :json
@@ -37,6 +45,7 @@ RSpec.describe TasksController do
     it "returns 429, if request count more than 100" do
       create \
         :request_ip,
+        source: source,
         ip_address: "60.241.160.246",
         count: 101
       post :create, params: { task: attributes_for(:task) }, format: :json
